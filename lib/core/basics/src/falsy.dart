bfc3a3edbfc2ed4ac54c0e3336f1da626aa3fc3a
Yczar <babalolagbogo@gmail.com>
2022-11-08 09:14:37 +0100

    chore: implemented the falsy feature

---
+++ lib/core/basics/src/falsy.dart
@@ -0,0 +1,17 @@
+import 'dart:math';
+
+const Set _falsyValues = {null, '', 0};
+
+/// Return a random falsy value (false, null, 0, '').
+/// [usages]
+/// falsy()
+/// falsy({ pool: [ NaN, undefined ] })
+dynamic falsy({
+  /// The default pool can be changed to better meet the needs:
+  Set? pool,
+}) {
+  if (pool != null) {
+    return pool.toList()[Random().nextInt(_falsyValues.length)];
+  }
+  return _falsyValues.toList()[Random().nextInt(_falsyValues.length)];
+}
