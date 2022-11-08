f090c7a71778203e7f2d40d336ad0f7254a57b1e
Yczar <babalolagbogo@gmail.com>
2022-11-08 02:08:57 +0100

    chore: created person.dart file

---
+++ lib/core/person/person.dart
@@ -0,0 +1,11 @@
+export 'src/aadhar.dart';
+export 'src/age.dart';
+export 'src/birthday.dart';
+export 'src/cf.dart';
+export 'src/first.dart';
+export 'src/gender.dart';
+export 'src/last.dart';
+export 'src/name.dart';
+export 'src/prefix.dart';
+export 'src/ssn.dart';
+export 'src/suffix.dart';
