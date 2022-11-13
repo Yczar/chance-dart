11e89879cd573d4969be3b05462c238308372550
Yczar <babalolagbogo@gmail.com>
2022-11-13 20:53:11 +0100

    docs: updated the basics docs with some features

--- lib/core/basics/src/falsy.dart
+++ lib/core/basics/src/falsy.dart
@@ -5,7 +5,7 @@ const Set _falsyValues = {null, '', 0};
 /// Return a random falsy value (false, null, 0, '').
 /// [usages]
 /// falsy()
-/// falsy({ pool: [ NaN, undefined ] })
+/// falsy({ pool: {null, '', 0} })
 dynamic falsy({
   /// The default pool can be changed to better meet the needs:
   Set? pool,
