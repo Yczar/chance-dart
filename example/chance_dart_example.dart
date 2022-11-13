3e1e7cabcbd862fcdf8dcccbfc9d3e3334670e41
Yczar <babalolagbogo@gmail.com>
2022-11-13 16:18:43 +0100

    chore: build runner test

--- example/chance_dart_example.dart
+++ example/chance_dart_example.dart
@@ -1,4 +1,14 @@
+import 'dart:mirrors';
+
+import 'package:chance_dart/chance_dart.dart';
+
 void main() {
+  testingMic();
   // var awesome = Awesome();
   // print('awesome: ${awesome.isAwesome}');
+}
+
+@Chance(chanceKey: ChanceKey.city)
+void testingMic() {
+  print(reflectClass(Chance).metadata.first.reflectee);
 }
--- lib/chance_dart.dart
+++ lib/chance_dart.dart
@@ -1,3 +1,51 @@
 library chance_dart;
 
 export 'core/core.dart';
+
+enum ChanceKey {
+  boolean,
+  character,
+  falsy,
+  floating,
+  integer,
+  letter,
+  natural,
+  prime,
+  string,
+  template,
+  ccType,
+  cc,
+  currencyPair,
+  currency,
+  dollar,
+  euro,
+  expMonth,
+  expYear,
+  exp,
+  naira,
+  address,
+  altitude,
+  areaCode,
+  city,
+  coordinates,
+  country,
+  depth,
+  geohash,
+  latitude,
+  locale,
+  longitude,
+  phone,
+  postal,
+  postCode,
+  province,
+  state,
+  street,
+  zip,
+}
+
+class Chance {
+  const Chance({
+    required this.chanceKey,
+  });
+  final ChanceKey chanceKey;
+}
