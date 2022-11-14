47550116db45837d4ddd2bb9b81d12fe7a0db6b6
Yczar <babalolagbogo@gmail.com>
2022-11-14 15:45:30 +0100

    docs: added docs to state

--- lib/core/location/src/state.dart
+++ lib/core/location/src/state.dart
@@ -1,5 +1,9 @@
 import '../../core.dart';
 
+/// Return the state of a generated address.
+///
+/// Returns:
+///   The state of the address generated.
 String state() {
   return address().state;
 }
