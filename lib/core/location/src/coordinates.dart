b4774279677ac9c43558dbaad283ab623b8aa71c
Yczar <babalolagbogo@gmail.com>
2022-11-14 15:43:42 +0100

    docs: added docs to coordinates

--- lib/core/location/src/coordinates.dart
+++ lib/core/location/src/coordinates.dart
@@ -2,6 +2,10 @@ import 'package:chance_dart/core/location/models/coordinate.dart';
 
 import '../../core.dart';
 
+/// Return the coordinates of the address of the user.
+/// 
+/// Returns:
+///   The coordinates of the address of the restaurant.
 Coordinate coordinates() {
   return address().coordinates;
 }
