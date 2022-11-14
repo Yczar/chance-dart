3d093beff2077fda3c5f04a7d7605f21a6bcd96c
Yczar <babalolagbogo@gmail.com>
2022-11-14 15:45:09 +0100

    docs: added docs to phone

--- lib/core/location/src/phone.dart
+++ lib/core/location/src/phone.dart
@@ -2,6 +2,13 @@ import 'dart:math';
 
 import 'package:chance_dart/chance_dart.dart';
 
+/// It generates a random phone number.
+///
+/// Args:
+///   temp (String): The template to use for the phone number.
+///
+/// Returns:
+///   A string of a phone number.
 String phone([String? temp]) {
   String phoneNumber;
   if (temp != null) {
