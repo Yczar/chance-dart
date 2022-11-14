dcdb901f1ed968e733a55d66daaa8fc135f42cea
Yczar <babalolagbogo@gmail.com>
2022-11-14 15:45:02 +0100

    docs: added docs to longitude

--- lib/core/location/src/longitude.dart
+++ lib/core/location/src/longitude.dart
@@ -1,5 +1,9 @@
 import '../../core.dart';
 
+/// Return the longitude of a randomly generated address.
+///
+/// Returns:
+///   The longitude of the address generared.
 double longitude() {
   return address().coordinates.longitude;
 }
