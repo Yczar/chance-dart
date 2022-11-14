a84753f929872f29eacecc48c61bce52ee1c4b15
Yczar <babalolagbogo@gmail.com>
2022-11-14 15:43:49 +0100

    docs: added docs to country

--- lib/core/location/src/country.dart
+++ lib/core/location/src/country.dart
@@ -5,6 +5,10 @@ import 'package:chance_dart/core/location/models/country.dart';
 
 import '../constants/countries.dart';
 
+/// It returns a random country from a list of countries.
+/// 
+/// Returns:
+///   A random country from the list of countries.
 Country country() {
   final decodedAddress = base64Decode(countries);
   final countryMap = jsonDecode(
