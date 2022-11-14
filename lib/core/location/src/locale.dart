7c3f8c6dd9c6f13c1569cad2bf45ab0d84e76991
Yczar <babalolagbogo@gmail.com>
2022-11-14 15:44:55 +0100

    docs: added docs to locale

--- lib/core/location/src/locale.dart
+++ lib/core/location/src/locale.dart
@@ -1,5 +1,9 @@
 import 'package:chance_dart/chance_dart.dart';
 
+/// It returns the state of a random address.
+///
+/// Returns:
+///   The state of the address.
 String locale() {
   return address().state;
 }
