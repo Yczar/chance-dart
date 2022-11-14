515861f2b145218258b96691e04068fefc9daf83
Yczar <babalolagbogo@gmail.com>
2022-11-14 12:42:24 +0100

    chore: added documentation for currency pair

--- lib/core/finance/src/currency_pair.dart
+++ lib/core/finance/src/currency_pair.dart
@@ -2,8 +2,16 @@ import 'package:chance_dart/chance_dart.dart';
 
 import '../models/currency.dart';
 
+/// Return a list of two currencies, where the second currency is not the same as the first currency.
+///
+/// Returns:
+///   A list of two currencies.
 List<Currency> currencyPair() {
+  /// creating a varible using the `currency` function from the `currency.dart` file.
   final currency1 = currency();
+
+  /// Calling the `currency` function from the `currency.dart` file, but passing in a parameter to
+  /// remove the first currency from the list of currencies.
   final currency2 = currency(currencyKeyToRemove: currency1.code);
   return [currency1, currency2];
 }
