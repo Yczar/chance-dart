0e4516af8a9f5d47d53da10bba40653325d2337d
Yczar <babalolagbogo@gmail.com>
2022-11-15 14:42:26 +0100

    docs: added docs to letter

--- lib/core/basics/src/letter.dart
+++ lib/core/basics/src/letter.dart
@@ -5,6 +5,8 @@ import 'character.dart';
 const String _lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
 const String _upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const String _chars = '$_lowerCaseChars$_upperCaseChars';
+
+///  a function that returns a random letter.
 String letter({
   /// Default includes both upper and lower case. It's possible to specify one or the other.
   Casing? casing,
@@ -17,6 +19,12 @@ String letter({
   return _getRandomString();
 }
 
+/// It generates a random string of length 1, using the characters in the string passed in as the first
+/// argument, or the default characters if no argument is passed in
+///
+/// Args:
+///   chars (String): The characters to use for the random string. If not provided, the default is all
+/// alphanumeric characters.
 String _getRandomString([String? chars]) => String.fromCharCodes(
       Iterable.generate(
         1,
