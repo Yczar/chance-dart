b845e04cce0d079919934895d445876434fc65c2
Yczar <babalolagbogo@gmail.com>
2022-11-15 15:01:38 +0100

    docs: added docs to second

--- lib/core/time/src/second.dart
+++ lib/core/time/src/second.dart
@@ -1,6 +1,9 @@
 import 'package:chance_dart/chance_dart.dart';
 
-// return any second of the minute
+/// Return a random integer between 0 and 60.
+///
+/// Returns:
+///   A random integer between 0 and 60.
 int second() {
   return integer(max: 60);
 }
