3f809c27645cc5d63604d2c4a903d28925e72340
Yczar <babalolagbogo@gmail.com>
2022-11-16 18:37:18 +0100

    docs: documented the age implementation

--- lib/core/person/src/age.dart
+++ lib/core/person/src/age.dart
@@ -1,5 +1,12 @@
 import 'package:chance_dart/chance_dart.dart';
-/// default max age is 123 but can be set in the parameter
+
+/// `age` returns a random integer between 0 and 123
+///
+/// Args:
+///   max (int): The maximum value of the integer.
+///
+/// Returns:
+///   A random integer between 0 and 123
 int age({int? max}) {
   return integer(max: max ?? 123);
 }
