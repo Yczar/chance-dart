c11270b72095fd2043c38af942af3b7c7d248fab
Peter Nathaniel <peternathaniel49@gmail.com>
2022-11-18 12:32:21 +0100

    test: write test for sentences and addedd comma to date, animal and word_test

--- test/core/text/word_test.dart
+++ test/core/text/word_test.dart
@@ -3,6 +3,11 @@ import 'package:chance_dart/core/text/constants/words.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('verify that word returned is from a list of words',
-      () => expect(word(), isIn(words)));
+  test(
+    'verify that word returned is from a list of words',
+    () => expect(
+      word(),
+      isIn(words),
+    ),
+  );
 }
--- test/core/thing/src/animal_test.dart
+++ test/core/thing/src/animal_test.dart
@@ -3,6 +3,11 @@ import 'package:chance_dart/core/thing/src/animal.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('verify that animal returned is random from animal list',
-      (() => expect(animal(), isIn(animals))));
+  test(
+    'verify that animal returned is random from animal list',
+    () => expect(
+      animal(),
+      isIn(animals),
+    ),
+  );
 }
