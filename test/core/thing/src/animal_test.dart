b66882df955cf31e9482ce5399a2ccacd2263d65
Peter Nathaniel <peternathaniel49@gmail.com>
2022-11-18 03:00:45 +0100

    test: write test for animal

---
+++ test/core/thing/src/animal_test.dart
@@ -0,0 +1,8 @@
+import 'package:chance_dart/core/thing/constants/animals.dart';
+import 'package:chance_dart/core/thing/src/animal.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('verify that animal returned is random from animal list',
+      (() => expect(animal(), isIn(animals))));
+}
