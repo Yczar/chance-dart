b29ec84bb5a225dee90ce63f998f0de29fbc4ac7
Yczar <babalolagbogo@gmail.com>
2022-11-20 17:44:40 +0100

    chore: created chance model

---
+++ chance_dart/example/chance_model.g.dart
@@ -0,0 +1,221 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'chance_model.dart';
+
+// **************************************************************************
+// TypeWrapperGenerator
+// **************************************************************************
+
+class ChanceModelWrapper {
+  ChanceModel generatedModel() {
+    return ChanceModel(
+      testing: true as bool,
+      geohash: r'brooks' as String,
+    );
+  }
+
+  List<ChanceModel> generatedModels() {
+    return [
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'elliott' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'beck' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'white' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'kelly' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'burke' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'hanson' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'simmons' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'cook' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'fitzgerald' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'cooper' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'woods' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'king' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'henderson' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'henderson' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'stevens' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'roberts' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'cook' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'brown' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'cook' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'peters' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'harris' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'fox' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'neal' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'bell' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'lawson' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'jacobs' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'freeman' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'nichols' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'russell' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'wade' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'reid' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'gibson' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'doyle' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'young' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'henderson' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'fuller' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'hoffman' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'long' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'martin' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'bates' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'hudson' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'hudson' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'wilson' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'hoffman' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'wallace' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'weaver' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'rhodes' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'flores' as String,
+      ),
+      ChanceModel(
+        testing: true as bool,
+        geohash: r'simmmons' as String,
+      ),
+      ChanceModel(
+        testing: false as bool,
+        geohash: r'nguyen' as String,
+      ),
+    ];
+  }
+}
