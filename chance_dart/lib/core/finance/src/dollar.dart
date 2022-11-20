5a7dc02fbb384e7cf7ce1c575ef1790870e8038d
Yczar <babalolagbogo@gmail.com>
2022-11-20 17:46:35 +0100

    chore: created dollar file

--- chance_dart/lib/core/finance/src/dollar.dart
+++ chance_dart/lib/core/finance/src/dollar.dart
@@ -1,12 +1,12 @@
 import 'package:chance_dart/chance_dart.dart';
 
 /// Generate a random dollar amount.
-/// 
+///
 /// Args:
 ///   max (int): The maximum value that can be generated.
 ///   min (int): The minimum value that can be generated.
 ///   fixed (int): The number of decimal places to return.
-/// 
+///
 /// Returns:
 ///   A string that starts with a dollar sign and ends with a number.
 String dollar({
