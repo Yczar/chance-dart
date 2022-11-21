18358e91ada620cb7705729b2d568c27e1e9c5c0
Yczar <babalolagbogo@gmail.com>
2022-11-21 17:54:56 +0100

    chore: update chance field

--- chance_dart/lib/src/annotations/src/chance_field.dart
+++ chance_dart/lib/src/annotations/src/chance_field.dart
@@ -3,8 +3,10 @@ import '../../../chance_dart.dart';
 class ChanceField {
   const ChanceField({
     required this.alias,
+    this.args = const {},
     this.defaultValue,
   });
   final ChanceAlias alias;
+  final Map<String, dynamic> args;
   final dynamic defaultValue;
 }
