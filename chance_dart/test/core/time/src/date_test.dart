499efbdf749799288c33846363ec2f038fb2ae95
Yczar <babalolagbogo@gmail.com>
2022-11-25 21:06:15 +0100

    fix: fix date test bug

--- chance_dart/test/core/time/src/date_test.dart
+++ chance_dart/test/core/time/src/date_test.dart
@@ -2,12 +2,16 @@ import 'package:chance_dart/core/time/time.dart';
 import 'package:test/test.dart';
 
 void main() {
+  final rndDate = date();
+  final parsedDate = DateTime.tryParse(rndDate);
+
   test(
-    'verify that year returned is the current date',
-    //18-11-2022 is the date as at when this test was done
+    'verify that parsed date is not equals to null',
     () => expect(
-      date(),
-      '18-11-2022',
+      parsedDate,
+      isNot(
+        equals(null),
+      ),
     ),
   );
 }
