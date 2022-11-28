360f2cdffe93b5a17da97213bb72981fabc65f84
0xfc2f <fawomath@gmail.com>
2022-11-28 11:08:15 +0100

    fix: fixed (#259) (#260)
    * fix: fix floating numbers generator
    
    * feat: added min value feature to integer numbers generator

--- packages/chance_dart/lib/core/basics/src/floating.dart
+++ packages/chance_dart/lib/core/basics/src/floating.dart
@@ -17,7 +17,8 @@ double floating({
   double randomNumber = Random().nextDouble();
   if (min != null || max != null) {
     randomNumber =
-        (Random().nextDouble() * ((max ?? 0) - (min ?? 0)) + (min ?? 0)).abs();
+        (Random().nextDouble() * ((max ?? 0) - (min ?? 0)).abs() + (min ?? 0))
+            .abs();
     if (fixed != null) {
       return double.parse(randomNumber.toStringAsFixed(fixed));
     }
--- packages/chance_dart/lib/core/basics/src/integer.dart
+++ packages/chance_dart/lib/core/basics/src/integer.dart
@@ -1,20 +1,23 @@
 import 'dart:math';
 
-/// This function returns a random integer between 0 and 1,000,000,000. If you
-///  pass in a max value, it
-/// will return a random integer between 0 and the max value.
+/// This function returns a random integer between min value and max value.
+/// If no argument if passed, it will generate random integers between
+/// 0 and 1,000,000,000.
 ///
 /// Args:
+///   min (int): The minimum number that can be generated.
 ///   max (int): The maximum number that can be generated.
 ///
 /// Returns:
-///   A random number between 0 and 1000000000
+///   A random number between min and max
 int integer({
+  int? min,
   int? max,
 }) {
   int randomNumber = Random().nextInt(1000000000);
-  if (max != null) {
-    randomNumber = Random().nextInt(max + 1);
+  if (min != null || max != null) {
+    randomNumber =
+        Random().nextInt(((max ?? 0) - (min ?? 0)).abs() + 1) + (min ?? 0);
 
     return randomNumber;
   }
